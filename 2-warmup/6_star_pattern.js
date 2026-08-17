ccf8a1127cd30a586a8b41ec12181a809c7a052c
Rohit Maurya <rohit139maurya@gmail.com>
2026-08-17 23:49:08 +0530

    day 2

--- 2-warmup/6_star_pattern.js
+++ 2-warmup/6_star_pattern.js
@@ -61,6 +61,7 @@
 
 //-------------------------------------------------------------------------------
 //Pattern 4
+// note : isme j i times chalta hai
 //  1
 //  2 2
 //  3 3 3
@@ -79,3 +80,71 @@
 // numStarPattern(5);
 
 //------------------------------------------------------------------------------
+//pattern 5
+// note : isme i n times chalta hai and j i times chalta hai
+//  1 2 3 4 5
+//  1 2 3 4
+//  1 2 3
+//  1 2
+//  1
+
+// function numStarPattern(n) {
+//   for (let i = n; i>0; i--) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//       row = row + " " + j;
+//     }
+//     console.log(row);
+//   }
+// }
+
+// numStarPattern(5);
+
+//---------------------------------------------------------------------------
+// pattern 6
+// note : isme i n times chalta hai and j i times chalta hai
+//  * * * * *
+//  * * * *
+//  * * *
+//  * *
+//  *
+// function starPattern(n) {
+//   for (let i = n; i > 0; i--) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//       row = row + " *";
+//     }
+//     console.log(row);
+//   }
+// }
+
+// starPattern(5);
+
+//------------------------------------------------------------------
+//pattern 7
+//     *
+//    **
+//   ***
+//  ****
+// *****
+// function reverseStartPattern(n){
+//     for (let i = 1; i <=n; i++) {
+//         let row = ""
+//         for (let k = 1; k <=n-i; k++) {
+//             row+=" "
+            
+//         }
+//         for (let j = 1; j <=i; j++) {
+//             row += "*"
+            
+//         }
+//         console.log(row);
+        
+        
+//     }
+// }
+
+// reverseStartPattern(5)
+
+
+//----------------------------------------------------------
